6b97fbeedbcdcb84bff5e47cc7a4b2a49aac0b9c
ballerburg9005 <github@ballerburg9005.33mail.com>
2021-08-14 10:34:20 +0200

    .

--- ecs/build.sh
+++ ecs/build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-current=$(date +%y.%m)
+current=$(date +%y.07)
 version=${1:-$current}
-
-docker build \
+#--platform linux/arm64,linux/amd64,linux/armhf \
+docker buildx build --platform linux/arm64,linux/amd64,linux/armhf \
     --build-arg VERSION=$version \
     --build-arg VCS_REF=`git rev-parse --short HEAD` \
     --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-    -t ejabberd/ecs:$version .
-
-[ "$version" = "latest" ] || docker tag ejabberd/ecs:$version ejabberd/ecs:latest
+    --push -t ballerburg9005/docker-ejabberd-ecs-official-arm .
+#[ "$version" = "latest" ] || docker tag ejabberd/ecs:$version ejabberd/ecs:latest
